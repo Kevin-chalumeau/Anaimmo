f3c0b3d9e44d37c8f0e6db5d189249b07a0e2b02
chalumeau <chalumeauk@gmail.com>
2020-08-03 16:03:22 +0200

    installation de webpack

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    /**
+     * @Route("/",name="home_index")
+     * @return Response A response instance
+     */
     public function index(): Response
     {
        return $this->render('index.html.twig');
