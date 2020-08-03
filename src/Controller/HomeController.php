716e2a426102e20b94f18b0c4c9aeb431a53fb95
chalumeau <chalumeauk@gmail.com>
2020-08-03 15:30:02 +0200

    1er controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,9 +1,13 @@
 <?php
-namespace App\Controller\HomeController;
+namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Response;
 
 class HomeController extends AbstractController
 {
-    
+    public function index(): Response
+    {
+       return $this->render('index.html.twig');
+    }
 }
