65926998790806bee97a9d03d4a1f51868341cc0
chalumeau <chalumeauk@gmail.com>
2020-08-12 10:19:12 +0200

    creation admin

--- migrations/Version20200812080651.php
+++ migrations/Version20200812080651.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200805113136 extends AbstractMigration
+final class Version20200812080651 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,16 @@ final class Version20200805113136 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE rent (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, city VARCHAR(255) NOT NULL, month_price INT NOT NULL, living_space INT NOT NULL, land_space INT NOT NULL, rooms INT NOT NULL, meuble TINYINT(1) NOT NULL, not_meuble TINYINT(1) NOT NULL, dpe VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE sale (id INT AUTO_INCREMENT NOT NULL, mandat_number INT NOT NULL, type LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', annonce_title VARCHAR(255) NOT NULL, price_fai INT NOT NULL, net_seller_price INT NOT NULL, pourcentage DOUBLE PRECISION NOT NULL, honorary INT NOT NULL, living_area INT NOT NULL, land_area INT NOT NULL, descriptif LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE rent');
         $this->addSql('DROP TABLE sale');
+        $this->addSql('DROP TABLE user');
     }
 }
---
+++ src/Entity/Rent.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RentRepository::class)
+ */
+class Rent
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $monthPrice;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $livingSpace;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $landSpace;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $rooms;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $meuble;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $notMeuble;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dpe;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getMonthPrice(): ?int
+    {
+        return $this->monthPrice;
+    }
+
+    public function setMonthPrice(int $monthPrice): self
+    {
+        $this->monthPrice = $monthPrice;
+
+        return $this;
+    }
+
+    public function getLivingSpace(): ?int
+    {
+        return $this->livingSpace;
+    }
+
+    public function setLivingSpace(int $livingSpace): self
+    {
+        $this->livingSpace = $livingSpace;
+
+        return $this;
+    }
+
+    public function getLandSpace(): ?int
+    {
+        return $this->landSpace;
+    }
+
+    public function setLandSpace(int $landSpace): self
+    {
+        $this->landSpace = $landSpace;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    public function getMeuble(): ?bool
+    {
+        return $this->meuble;
+    }
+
+    public function setMeuble(bool $meuble): self
+    {
+        $this->meuble = $meuble;
+
+        return $this;
+    }
+
+    public function getNotMeuble(): ?bool
+    {
+        return $this->notMeuble;
+    }
+
+    public function setNotMeuble(bool $notMeuble): self
+    {
+        $this->notMeuble = $notMeuble;
+
+        return $this;
+    }
+
+    public function getDpe(): ?string
+    {
+        return $this->dpe;
+    }
+
+    public function setDpe(string $dpe): self
+    {
+        $this->dpe = $dpe;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
