f3c5855309f7ced92b60d1ee3c298ae3bdf83af0
chalumeau <chalumeauk@gmail.com>
2020-09-21 11:56:12 +0200

    fin SearchBien

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,18 +1,46 @@
-<?php
+<?php 
 namespace App\Controller;
 
+use App\Form\SearchBienType;
+use App\Model\SearchBien;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 class SearchController extends AbstractController
 {
-    /**
-     * @Route("/search",name="search_index")
-     * @return Response A response instance
+   /**
+     * Contact page display
+     * @Route("/search",name="search_index",  methods={"GET","POST"})
+     * @return Response A reponse instance
      */
-    public function index(): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
-       return $this->render('search/index.html.twig');
+        $searchBien = new SearchBien();
+        $form = $this->createForm(SearchBienType::class, $searchBien);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new templatedEmail())
+                ->from($this->getParameter('mailer_from'))
+                ->to(new Address($this->getParameter('mailer_to')))
+                ->subject("Vous avez reçu un email pour une recherche de bien !")
+                ->html($this->renderView('search/mail.html.twig', [
+                    'searchBien' => $searchBien
+                ]));
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été transmis, nous vous répondrons dans les meilleurs délais.');
+            return $this->redirectToRoute('search_index');
+        }
+
+        return $this->render('search/index.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Model/SearchBien.php
+++ src/Model/SearchBien.php
@@ -1,8 +1,47 @@
 <?php
 namespace App\Model;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class SearchBien
-{
+{   
+    /**
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 100,
+     *      minMessage = "Votre nom doit avoir plus de deux caractères",
+     * )
+     * @Assert\Type("string")
+     * @Assert\NotBlank(message ="Le champ doit être obligatoirement rempli")
+     */
+    private $firstname;
+
+    /**
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 100,
+     *      minMessage = "Votre prénom doit avoir plus de deux caractères",
+     * )
+     * @Assert\Type("string")
+     * @Assert\NotBlank(message ="Le champ doit être obligatoirement remplis")
+     */
+    private $lastname;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Type("string")
+     * @Assert\Regex(
+     *  pattern="/[0-9]{10}/")
+     */
+    private $phone;
+
+    /**
+     * @Assert\Type("string")
+     * @Assert\Email(
+     *      message = "L'adresse mail n'est une adresse valide")
+     * @Assert\NotBlank
+     */
+    private $email;
     private $type;
 
     private $budget;
@@ -15,7 +54,87 @@ class SearchBien
 
     private $landSpace;
 
+    /**
+     * @Assert\Length(
+     *      min = 1,
+     *      max = 1500,
+     *      maxMessage = "Le champ ne doit pas dépasser 1500 caractères")
+     * @Assert\NotBlank(message = "Le champ doit obligatoirement être rempli")
+     */
     private $message;
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     * @return SearchBien
+     */
+    public function setFirstname(string $firstname): Searchbien
+    {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     * @return SearchBien
+     */
+    public function setLastname(string $lastname): SearchBien
+    {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return SearchBien
+     */
+    public function setEmail(string $email): SearchBien
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     * @return SearchBien
+     */
+    public function setPhone(string $phone): SearchBien
+    {
+        $this->phone = $phone;
+        return $this;
+    }
+
 
     /**
      * @return string
@@ -125,4 +244,21 @@ class SearchBien
         return $this;
     }
 
+     /**
+     * @return string
+     */
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return SearchBien
+     */
+    public function setMessage(string $message): SearchBien
+    {
+        $this->message = $message;
+        return $this;
+    }
 }
